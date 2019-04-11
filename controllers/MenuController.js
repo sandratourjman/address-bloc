c891dd574a0cd0c2b54df687aaf66c655fef1981
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2019-04-10 22:43:39 -0500

    Checkpoint 3

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const ContactController = require("./ContactController");
+
 
 module.exports = class MenuController {
 	constructor(){
@@ -14,7 +16,7 @@ module.exports = class MenuController {
 				]
 			}
 		];
-		this.contacts = [];
+		this.book = new ContactController();
 	}
 
 	main(){
@@ -45,8 +47,15 @@ module.exports = class MenuController {
 
 	addContact(){
 		this.clear();
-		console.log('addContact called');
-		this.main();
+		inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+			this.book.addContact(answers.name, answers.phone).then((contact) => {
+	        console.log("Contact added successfully!");
+	        this.main();
+	       }).catch((err) => {
+	         console.log(err);
+	         this.main();
+	       });
+		});
 	}
 
 	getDate(){
